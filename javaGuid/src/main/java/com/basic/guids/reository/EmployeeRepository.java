ebc88d8e83a3ce580f994eb83eb972ab04b64eb1
DeepakKannan1 <116724882+DeepakKannan1@users.noreply.github.com>
2023-05-15 12:36:53 +0530

    Delete javaGuid directory

--- javaGuid/src/main/java/com/basic/guids/reository/EmployeeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.basic.guids.reository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.basic.guids.model.Employee;
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
-
-}
